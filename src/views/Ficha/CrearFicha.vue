727a7daf7e702004d0bee5b34fb0f8a449e2623d
jrealesdelaossa <jrealesdelaossa@gmail.com>
2023-06-29 23:37:23 -0500

    primer boceto de ficha

--- src/views/Ficha/CrearFicha.vue
+++ src/views/Ficha/CrearFicha.vue
@@ -1,9 +1,8 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <v-container>
     <v-row justify="space-around">
       <v-card width="800">
-        <v-app-bar flat color="rgba(156, 216, 175, 256)">
+        <v-app-bar flat color="rgb(52,188,52)">
           <v-toolbar-title class="text-h6 white--text pl-0">
             CREAR FICHA
           </v-toolbar-title>
@@ -17,15 +16,13 @@
               <v-row>
                 <v-col>
                   <v-row>
-                    <v-row class="mb-4">
-                      <v-col cols="6">
-                        <v-text-field
-                          label="Código"
-                          append-icon="mdi-key"
-                          v-model="paquete.codigo"
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
+                    <v-col cols="6">
+                      <v-text-field
+                        label="Código"
+                        append-icon="mdi-key"
+                        v-model="paquete.codigo"
+                      ></v-text-field>
+                    </v-col>
                   </v-row>
 
                   <v-row cols="12" class="mx-2">
@@ -34,13 +31,16 @@
                       v-model="paquete.startDate"
                       type="date"
                       label="Fecha de inicio"
+                      @input="validarFecha"
                       outlined
                     ></v-text-field>
                     <v-text-field
+                      class="ml-2"
                       v-model="paquete.endDate"
                       type="date"
                       label="Fecha de fin"
                       outlined
+                      @input="validarFecha"
                     ></v-text-field>
                   </v-row>
 
@@ -51,7 +51,7 @@
                         item-text="departamento"
                         item-value="departamento"
                         label="Selecciones Ambiente"
-                        v-model="paquete.departamento"
+                        v-model="paquete.ambiente"
                         append-icon="school"
                       ></v-select>
                     </v-col>
@@ -61,7 +61,7 @@
                         item-text="departamento"
                         item-value="departamento"
                         label="Seleccione Programa"
-                        v-model="paquete.departamento"
+                        v-model="paquete.programa"
                         append-icon="book"
                       ></v-select>
                     </v-col>
@@ -74,7 +74,7 @@
                         item-text="departamento"
                         item-value="departamento"
                         label="Seleccione Intructor"
-                        v-model="paquete.departamento"
+                        v-model="paquete.instructor"
                         append-icon="mdi-account"
                       ></v-select>
                     </v-col>
@@ -83,21 +83,21 @@
                   <v-row cols="12">
                     <v-col cols="6">
                       <v-select
-                        :items="departamentos"
-                        item-text="departamento"
-                        item-value="departamento"
+                        :items="diasSemana"
+                        item-text="diasSemana"
+                        item-value="diasSemana"
                         label="Seleccione Día"
-                        v-model="paquete.departamento"
+                        v-model="paquete.dia"
                         append-icon="mdi-calendar"
                       ></v-select>
                     </v-col>
                     <v-col cols="6">
                       <v-select
-                        :items="departamentos"
-                        item-text="departamento"
-                        item-value="departamento"
+                        :items="jornada"
+                        item-text="jornadas"
+                        item-value="jornadas"
                         label="Seleccione Jornada"
-                        v-model="paquete.departamento"
+                        v-model="paquete.jornada"
                         append-icon="mdi-calendar"
                       ></v-select>
                     </v-col>
@@ -107,48 +107,104 @@
             </v-container>
           </v-form>
         </v-card-text>
-        <v-card-actions>
-          <v-btn class="ma-2" outlined color="indigo" @click="guardar()">
+        <v-card-actions class="mx-12">
+          <v-btn class="ma-2" color="secondary" @click="agregarLista">
+            Agregar
+          </v-btn>
+          <v-btn class="ma-2" color="rgb(52,188,52)" @click="guardar()">
             Crear
           </v-btn>
         </v-card-actions>
         <v-data-table
-          :items="tablaDatos"
+          :items="listItems"
           :headers="tablaHeaders"
-          class="elevation-1"
+          class="elevation-1 mx-12 my-6"
         ></v-data-table>
+
+        <table class="table table-white mx-12 my-6">
+          <thead>
+            <tr>
+              <th v-for="title in titles" :key="title">
+                {{ title }}
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="item in listItems" :key="item.dia">
+              <td>{{ item.dia }}</td>
+
+              <td>{{ item.jornada }}</td>
+            </tr>
+          </tbody>
+        </table>
       </v-card>
     </v-row>
+
+    <!-- Modal de advertencias y errores -->
+    <!-- <v-btn @click="mostrarModal">Mostrar Modal</v-btn> -->
+    <v-dialog v-model="mostrar" max-width="500px" class="">
+      <v-card>
+        <v-card-title>
+          <span class="headline text-lg-h4">{{ modalTitle }}</span>
+        </v-card-title>
+        <v-card-text class="text-lg-h6"> {{ modalText }} </v-card-text>
+        <v-card-actions>
+          <v-btn color="error" @click="cerrarModal">Cerrar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <pre>
-        {{ $data }}
-      </pre
+          {{ $data }}
+        </pre
     >
   </v-container>
 </template>
 
 <script>
 import axios from "axios";
+import dia from "../../json/dia";
+import jornadas from "../../json/jornada";
 
 export default {
   props: {
     datos: Object,
-    mostrar: Boolean,
+    // mostrar: Boolean,
   },
   data() {
     return {
       paquete: {
-        nombre: null,
-        nomenclatura: null,
+        codigo: null,
         startDate: null,
         endDate: null,
+        ambiente: null,
+        programa: null,
+        instructor: null,
+        dia: null,
+        jornada: null,
       },
+      // titulos de la primera tabla
+      tablaHeaders: [
+        { text: "Día", value: "dia" },
+        { text: "Jornada", value: "jornada" },
+      ],
+      // titulos de la segunda tabla
+      titles: ["Día", "Jornada"],
+      // items de la tabla
+      listItems: [],
+      diasSemana: null,
+      jornada: null,
+      mostrar: false,
+      modalTitle: "",
+      modalText: "",
     };
   },
 
   methods: {
     async guardar() {
       await axios
-        .post("http://10.187.145.190:3000/centro/crear", this.paquete)
+        // TODO: Agregar el endpoint
+        .post("http://159.223.110.82:3000/", this.paquete)
         .then(function (response) {
           console.log(response);
         })
@@ -160,7 +216,66 @@ export default {
           // always executed
         });
     },
+
+    agregarLista() {
+      if (this.paquete.dia == null || this.paquete.jornada == null) {
+        this.mostrarModal("Error", "Debe seleccionar un día y una jornada");
+        return;
+      }
+
+      this.listItems.push({
+        dia: this.paquete.dia,
+        jornada: this.paquete.jornada,
+      });
+
+      this.paquete.dia = null;
+      this.paquete.jornada = null;
+    },
+
+    validarFecha() {
+      const fechaInicio = this.paquete.startDate.split("-");
+      const fechaFin = this.paquete.endDate.split("-");
+
+      const fechaInicioObj = new Date(
+        fechaInicio[0],
+        fechaInicio[1] - 1,
+        fechaInicio[2]
+      );
+      const fechaFinObj = new Date(fechaFin[0], fechaFin[1] - 1, fechaFin[2]);
+
+      // Verifica si las fechas son válidas
+      if (
+        isNaN(fechaInicioObj.getTime()) ||
+        isNaN(fechaFinObj.getTime()) ||
+        fechaInicioObj > fechaFinObj
+      ) {
+        this.paquete.startDate = null;
+        this.paquete.endDate = null;
+        this.mostrarModal(
+          "Error",
+          "La fecha de inicio debe ser menor a la fecha de fin"
+        );
+        return false; // Las fechas no son válidas
+      }
+
+      return true; // Las fechas son válidas
+    },
+
+    mostrarModal(title, message) {
+      this.modalTitle = title;
+      this.modalText = message;
+      this.mostrar = true;
+    },
+    cerrarModal() {
+      this.mostrar = false;
+    },
   },
+
+  created() {
+    this.diasSemana = dia;
+    this.jornada = jornadas;
+  },
+
   async mounted() {
     const response = await axios.get("http://10.187.145.190:3000/regional");
     this.regionales = response.data;
